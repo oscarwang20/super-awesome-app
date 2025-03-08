c35a2bd3518218945ca29c9ddeabdeadc743f143
Oscar Wang <oscarwang22396@gmail.com>
2025-03-08 14:29:08 -0500

    add service account

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,6 +1,6 @@
 import admin, { ServiceAccount } from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "./serivce_account.json";
+import serviceAccount from "./serivce-account.json";
 import { cert } from "firebase-admin/app";
 
 const app = admin.initializeApp({

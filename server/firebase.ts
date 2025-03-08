e4329cf10117e2aff56a258a8db2833d12c3e09e
Oscar Wang <oscarwang22396@gmail.com>
2025-03-08 14:30:32 -0500

    updates

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,6 +1,6 @@
 import admin, { ServiceAccount } from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "./serivce-account.json";
+import serviceAccount from "./service-account.json";
 import { cert } from "firebase-admin/app";
 
 const app = admin.initializeApp({
